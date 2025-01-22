19087bcbd781a816b95b73fc6629e1a4a47452be
Najib Hossain Nahin <najibnahin.dev@gmail.com>
2025-01-22 23:31:05 +0600

    01 02

---
+++ Week 2.0 - String, Class, Objects/Module 5.0 - String Class in C++/01 string.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "Hello";  //we can use string as data type
+    cout << s << endl;
+
+    s = "Gello";  //can directly change the input
+    cout << s << endl;
+
+    string s0 = "Gello";
+    if(s == s0) cout << "Same";  //can directly compare
+    else cout << "Not Same";
+
+
+    return 0;
+}
