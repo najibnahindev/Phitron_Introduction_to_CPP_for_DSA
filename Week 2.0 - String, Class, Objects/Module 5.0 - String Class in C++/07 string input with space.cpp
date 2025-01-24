0173d1848d45919c915f9df3fdf504847a036a24
Najib Hossain Nahin <najibnahin.dev@gmail.com>
2025-01-25 04:52:51 +0600

    07

---
+++ Week 2.0 - String, Class, Objects/Module 5.0 - String Class in C++/07 string input with space.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    string s;
+    cin.ignore();
+    getline(cin, s);  //input a line
+
+    cout << n <<endl;
+    cout << s <<endl;
+
+    return 0;
+}
