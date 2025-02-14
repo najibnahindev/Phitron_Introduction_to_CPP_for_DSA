297b5f1bcd22d42c957bb1eb13696d0c92306fb9
Najib Hossain Nahin <najibnahin.dev@gmail.com>
2025-02-15 00:43:15 +0600

    done

--- Week 1.0 - Introduction to C++/Module 3.5 - Week 1.0 Practice Day 2.0/07 practice problem 2.0.cpp
+++ Week 1.0 - Introduction to C++/Module 3.5 - Week 1.0 Practice Day 2.0/07 practice problem 2.0.cpp
@@ -12,16 +12,12 @@ class Cricketer
         this->jersey = jersey;
     }
 };
-using namespace std;
 int main()
 {
     Cricketer* nasir =  new Cricketer("Bangladesh", 69);
     Cricketer* boult =  new Cricketer("Newzeland", 96);
 
-    // nasir->country = boult->country;  //proper way to copy and we won't use nasir = boult
-    // nasir->jersey = boult->jersey;
-
-    *nasir = *boult;  //we can simply use this instead of line 21 and 22
+    *nasir = *boult;
 
     delete boult;
     cout << nasir->country << " " << nasir->jersey << endl;
